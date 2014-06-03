cf904ed6eba798113bf12e22ae643d3aced81c9f
Rob Rix <rob.rix@github.com>
2014-06-03 14:21:09 -0700

    Initial Commit

---
+++ Hammer/Hammer.h
@@ -0,0 +1,19 @@
+//
+//  Hammer.h
+//  Hammer
+//
+//  Created by Rob Rix on 2014-06-03.
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Hammer.
+FOUNDATION_EXPORT double HammerVersionNumber;
+
+//! Project version string for Hammer.
+FOUNDATION_EXPORT const unsigned char HammerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hammer/PublicHeader.h>
+
+
