bd1f87520d39c7bd4d3d1b4098b22b4fb5a09972
Rob Rix <rob.rix@github.com>
2014-06-22 12:11:10 -0400

    Document boxing.

--- Hammer/Box.swift
+++ Hammer/Box.swift
@@ -1,9 +1,13 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
+/// Boxes \c value up for use in recursive structs/enums or parameterized classes.
+///
+/// This is a currently-required implementation detail to handle the compiler’s lack of codegen for recursive enum/struct definitions, and classes with non-fixed layout.
 func box<T>(value: T) -> Box<T> {
 	return Box(value: value)
 }
 
+/// A box for a value which would otherwise cause compiler issues: recursive enum/struct definitions, and classes with non-fixed layout.
 @final class Box<T> {
 	let _value: T[]
 	
