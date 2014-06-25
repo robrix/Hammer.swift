d621e6590c9e27468f42f7a7103498562e3c3d99
Rob Rix <rob.rix@github.com>
2014-06-25 00:20:56 -0400

    Implement reduce over languages.

---
+++ Hammer/Reduce.swift
@@ -0,0 +1,41 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+/// Traverse a recursive language.
+func reduce<Into, Alphabet>(language: Language<Alphabet>, initial: Into, combine: (Into, Language<Alphabet>) -> Into) -> Into {
+	var memo = Dictionary<Language<Alphabet>, Into>()
+	return _reduce(memo, language, initial, combine)
+}
+
+func _reduce<Into, Alphabet>(var memo: Dictionary<Language<Alphabet>, Into>, language: Language<Alphabet>, initial: Into, combine: (Into, Language<Alphabet>) -> Into) -> Into {
+	if let cached = memo[language] { return cached }
+	
+	memo[language] = initial
+	var into = combine(initial, language)
+	memo[language] = into
+	
+	switch language {
+		case .Empty:
+			fallthrough
+		case .Null:
+			fallthrough
+		case .Literal:
+			break
+		
+		case let .Alternation(left, right):
+			into = _reduce(memo, left, into, combine)
+			into = _reduce(memo, right, into, combine)
+		
+		case let .Concatenation(first, second):
+			into = _reduce(memo, first, into, combine)
+			into = _reduce(memo, second, into, combine)
+		
+		
+		case let .Repetition(language):
+			into = _reduce(memo, language, into, combine)
+		
+		case let .Reduction(language, _):
+			into = _reduce(memo, language, into, combine)
+	}
+	
+	return into
+}
