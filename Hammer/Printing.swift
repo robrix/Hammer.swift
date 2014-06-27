1ff0bbf9e5df5c502c7e49fbd118d8ce0a8edf42
Rob Rix <rob.rix@github.com>
2014-06-27 16:06:00 -0400

    Combinators are Printable.

--- Hammer/Printing.swift
+++ Hammer/Printing.swift
@@ -1,12 +1,20 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
-extension Language : Printable {
-	/// Pretty-prints the language.
+/// Combinator conforms to Printable.
+extension Combinator : Printable {
 	var description: String {
-		switch self {
+		return describe(self)
+	}
+}
+
+
+/// Pretty-prints the language represented by a combinator.
+func describe<Alphabet : protocol<Hashable, Printable>>(combinator: Combinator<Alphabet>) -> String {
+	let describe: Combinator<Alphabet> -> String = fixpoint("") { recur, combinator in
+		switch combinator.language {
 		case .Empty:
 			return "∅"
-		
+			
 		case let .Null(parses):
 			let joined = join(" ", map(parses) { return $0.description })
 			return "ε↓{\(joined)}"
@@ -17,24 +25,25 @@ extension Language : Printable {
 			
 			
 		case let .Alternation(left, right):
-			return "\(left) ∪ \(right)"
+			return "\(recur(left)) ∪ \(recur(right))"
 			
 //		case let .Concatenation(first, .Repeat(second)) where first == second:
-//			return "\(first)+"
+//			return "\(recur(first))+"
 			
 		case let .Concatenation(first, second):
-			return "\(first) ✕ \(second)"
-			
+			return "\(recur(first)) ✕ \(recur(second))"
+
 //		case let .Intersection(left, right):
-//			return "\(left) ∩ \(right)"
+//			return "\(recur(left)) ∩ \(recur(right))"
 			
 			
 		case let .Repetition(language):
-			return "\(language)*"
+			return "\(recur(language))*"
 			
 			
 		case let .Reduction(language, _):
-			return "\(language) → 𝑓"
+			return "\(recur(language)) → 𝑓"
 		}
 	}
+	return describe(combinator)
 }
