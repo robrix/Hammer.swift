3d7ee3b9eb2d9a073358e998cb8a54e8cc819b6c
Rob Rix <rob.rix@github.com>
2014-06-28 08:20:01 -0400

    Flesh out the definition of nullability.

--- Hammer/Nullability.swift
+++ Hammer/Nullability.swift
@@ -1,5 +1,27 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
+/// Returns whether or not the receiver can parse the empty string.
 func nullable<Alphabet : Alphabet>(combinator: Combinator<Alphabet>) -> Bool {
-	return false
+	let nullable: Combinator<Alphabet> -> Bool = fixpoint(false) { recur, combinator in
+		switch combinator.language {
+		case .Null:
+			return true
+			
+		case let .Alternation(left, right):
+			return recur(left) || recur(right)
+			
+		case let .Concatenation(first, second):
+			return recur(first) && recur(second)
+			
+		case .Repetition:
+			return true
+			
+		case let .Reduction(c, _):
+			return recur(c)
+			
+		default:
+			return false
+		}
+	}
+	return nullable(combinator)
 }
