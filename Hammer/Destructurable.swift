ec68600c0cae9c51f76fc99bbe636eb2218edda8
Rob Rix <rob.rix@github.com>
2014-07-01 09:04:01 -0400

    Quick stab at recursive destructuring.

--- Hammer/Destructurable.swift
+++ Hammer/Destructurable.swift
@@ -5,3 +5,25 @@ protocol Destructurable {
 	typealias Pattern
 	func destructure() -> Pattern
 }
+
+
+func destructure2<Alphabet : Alphabet>(language: Language<Alphabet, Combinator<Alphabet>>) -> Language<Alphabet, Language<Alphabet, Combinator<Alphabet>>> {
+	switch language {
+	case .Empty:
+		return .Empty
+		
+	case let .Null(x):
+		return .Null(x)
+		
+	case let .Literal(x):
+		return .Literal(x)
+		
+//	case let .Alternation(x, y):
+//		// fixme: this should unpack to the alternation of the evaluation of x and y
+//		// fixme: need to delay these or put the delay in Recur
+//		return .Alternation(x.forced.language, y.forced.language)
+//		
+	default:
+		return .Empty
+	}
+}
