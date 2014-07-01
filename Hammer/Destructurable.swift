86d3b023bb1e3a5caf197cfedfdaeb3eb31340fb
Rob Rix <rob.rix@github.com>
2014-07-01 10:08:37 -0400

    Flesh out destructuring of the rest of the combinators.

--- Hammer/Destructurable.swift
+++ Hammer/Destructurable.swift
@@ -18,12 +18,17 @@ func destructure<Alphabet : Alphabet>(language: Language<Alphabet, Combinator<Al
 	case let .Literal(x):
 		return .Literal(x)
 		
-//	case let .Alternation(x, y):
-//		// fixme: this should unpack to the alternation of the evaluation of x and y
-//		// fixme: need to delay these or put the delay in Recur
-//		return .Alternation(x.forced.language, y.forced.language)
-//		
-	default:
-		return .Empty
+	// fixme: these should unpack to the alternation of the evaluation of x and y, rather than the delay of those, but we cannot currently put the delay in Recur rdar://17520072
+	case let .Alternation(x, y):
+		return .Alternation(delay(x.forced.language), delay(y.forced.language))
+		
+	case let .Concatenation(x, y):
+		return .Concatenation(delay(x.forced.language), delay(y.forced.language))
+		
+	case let .Repetition(x):
+		return .Repetition(delay(x.forced.language))
+		
+	case let .Reduction(x, f):
+		return .Reduction(delay(x.forced.language), f)
 	}
 }
