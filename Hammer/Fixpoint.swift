6f8f7845a67b4b408f80b7c28a9d9cb4be1d117c
Rob Rix <rob.rix@github.com>
2014-07-02 23:40:59 -0400

    HashablePairs wrap Identifiables.

--- Hammer/Fixpoint.swift
+++ Hammer/Fixpoint.swift
@@ -4,7 +4,7 @@ func fixpoint<Parameter : Identifiable, Result> (initial: Result, body: (Paramet
 	return fixpoint(initial, { $0.identity }, body)
 }
 
-func fixpoint<Parameter : Hashable, Result> (initial: Result, body: ((Parameter, Parameter) -> Result, (Parameter, Parameter)) -> Result) -> (Parameter, Parameter) -> Result {
+func fixpoint<Parameter : Identifiable, Result> (initial: Result, body: ((Parameter, Parameter) -> Result, (Parameter, Parameter)) -> Result) -> (Parameter, Parameter) -> Result {
 	return fixpoint(initial, { HashablePair($0.0, $0.1) }, body)
 }
 
@@ -31,7 +31,7 @@ func fixpoint<Parameter, Result, Decorator : Hashable>(initial: Result, wrap: Pa
 
 
 /// A pair which distributes hashing and equality over its members. This is an implementation detail.
-struct HashablePair<T : Hashable, U : Hashable> {
+struct HashablePair<T : Identifiable, U : Identifiable> {
 	let left: T
 	let right: U
 	
@@ -50,12 +50,12 @@ func identify<T>(a: T) -> ObjectIdentifier? {
 
 /// Distribute equality over hashable pairs.
 func == <T, U> (a: HashablePair<T, U>, b: HashablePair<T, U>) -> Bool {
-	return identify(a.left)! == identify(b.left)! && identify(a.right)! == identify(b.right)!
+	return a.left.identity == b.left.identity && a.right.identity == b.right.identity
 }
 
 extension HashablePair : Hashable {
 	// This is a poor way to distribute hashing over a pair; it’s convenient, but it’s not a good implementation detail to rely upon or emulate.
 	var hashValue: Int {
-		return identify(left)!.hashValue ^ identify(right)!.hashValue
+		return left.identity.hashValue ^ right.identity.hashValue
 	}
 }
