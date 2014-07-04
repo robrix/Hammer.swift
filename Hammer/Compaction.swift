df78a02af5046a6ae047c155629b4ac0ff5c0b27
Rob Rix <rob.rix@github.com>
2014-07-03 21:43:12 -0400

    Remove the old function for compaction.

--- Hammer/Compaction.swift
+++ Hammer/Compaction.swift
@@ -3,49 +3,10 @@
 import List
 import Set
 
-/// Returns a combinator equivalent to \c combinator but more compact.
-///
-/// If the language cannot be compacted, it is returned unchanged.
-func compact<Alphabet : Alphabet>(combinator: Combinator<Alphabet>) -> Combinator<Alphabet> {
-	let compact: Combinator<Alphabet> -> Combinator<Alphabet> = fixpoint(combinator) { recur, combinator in
-		switch combinator.language {
-		
-		/// Alternations with Empty are equivalent to the other alternative.
-		case let .Alternation(x, y) where recur(x).language == .Empty:
-			return recur(y)
-		case let .Alternation(x, y) where recur(y).language == .Empty:
-			return recur(x)
-		
-		/// Concatenations with Empty are equivalent to Empty.
-		case let .Concatenation(x, y) where recur(x).language == .Empty || recur(y).language == .Empty:
-			return Combinator(.Empty)
-		
-		/// Repetitions of empty cannot parse.
-		case let .Repetition(x) where recur(x).language == .Empty:
-			// fixme: how does this even work? List() is not in Alphabet.
-			return Combinator(.Null(Set(List())))
-			
-		/// Reductions of reductions compose.
-		case let .Reduction(x, f):
-//			switch recur(x).language {
-//			case let .Reduction(y, g):
-//				let composition: Alphabet -> Any = compose(g, f)
-//				return Combinator(.Reduction(y, composition))
-//			default:
-//				break
-//			}
-			fallthrough
-		
-		default:
-			return combinator
-		}
-	}
-	return compact(combinator)
-}
-
-
-
 extension Combinator {
+	/// Returns a combinator equivalent to \c combinator but more compact.
+	///
+	/// If the language cannot be compacted, it is returned unchanged.
 	func compact() -> Combinator<Alphabet> {
 		switch self.destructure() {
 		/// Alternations with Empty are equivalent to the other alternative.
