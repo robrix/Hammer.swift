7216f0c543422f25413d25d49ab46d96e360a488
Rob Rix <rob.rix@github.com>
2014-07-04 01:14:17 -0400

    Add some commented-out tests.

--- Hammer/Parsing.swift
+++ Hammer/Parsing.swift
@@ -15,5 +15,9 @@ struct ParsingTests : Testable {
 	static func _performTests() {
 		let xs = Combinator(literal: "x")*
 		assert(xs.parse([]).count == 0)
+//		assert(xs.parse([""]).count == 0)
+//		let parsedX = xs.parse(["x"])
+//		println(parsedX)
+//		assert(parsedX.count == 1)
 	}
 }
