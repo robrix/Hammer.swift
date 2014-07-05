40844ed602bad3800f9f60b841c86e19777ff48a
Rob Rix <rob.rix@github.com>
2014-07-05 12:55:12 -0400

    Replace binary trees with parse trees.

---
+++ Hammer/ParseTree.swift
@@ -0,0 +1,65 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+import Set
+
+/// A (potentially ambiguous, but otherwise binary) parse tree.
+enum ParseTree<T : Hashable> {
+	/// Represents the termination of a branch (list).
+	case Nil
+	
+	/// Represents a leaf node.
+	case Leaf(Box<T>)
+	
+	/// Represents a binary branch node.
+	///
+	/// S-list–style constructions use this, coupled with .Leaf() and .Nil.
+	case Branch(Box<ParseTree<T>>, Box<ParseTree<T>>)
+	
+	/// Represents an ambiguity in the parse tree.
+	case Choice(Set<ParseTree<T>>)
+}
+
+
+/// ParseTree conforms to Equatable.
+func == <T> (a: ParseTree<T>, b: ParseTree<T>) -> Bool {
+	switch (a, b) {
+	case (.Nil, .Nil):
+		return true
+		
+	case let (.Leaf(x), .Leaf(y)) where x == y:
+		return true
+		
+	case let (.Branch(x1, y1), .Branch(x2, y2)) where x1.value == x2.value && y1.value == y2.value:
+		return true
+		
+	case let (.Choice(x), .Choice(y)) where x == y:
+		return true
+		
+	default:
+		return false
+	}
+}
+
+
+/// ParseTree conforms to Hashable.
+extension ParseTree : Hashable {
+	var hashValue: Int {
+		switch self {
+		case .Nil:
+			return 0
+			
+		case let .Leaf(x):
+			return x.value.hashValue
+			
+		case let .Branch(x, y):
+			return x.value.hashValue ^ y.value.hashValue
+			
+		case let .Choice(x):
+			return x.hashValue
+		}
+	}
+}
+
+
+// fixme: cons
+// fixme: cartesian product
