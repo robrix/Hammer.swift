b5383faab364559342862010630c7f500b9b3742
Rob Rix <rob.rix@github.com>
2014-07-07 17:07:05 -0400

    Documentation.

--- Hammer/Generator.swift
+++ Hammer/Generator.swift
@@ -1,5 +1,6 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
+/// Concatenation of Generators.
 func ++ <A : Generator, B : Generator, Element where Element == A.Element, Element == B.Element>(var a: A, var b: B) -> GeneratorOf<Element> {
 	return GeneratorOf {
 		switch a.next() {
