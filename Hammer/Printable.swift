b8a9b4511dcb7abe75c8f903f7a05d5ee00a190c
Rob Rix <rob.rix@github.com>
2014-07-07 18:26:29 -0400

    description is just a computed property.

--- Hammer/Printable.swift
+++ Hammer/Printable.swift
@@ -2,45 +2,40 @@
 
 /// Combinator conforms to Printable.
 extension Combinator : Printable {
+	/// Pretty-prints the language represented by a combinator.
 	var description: String {
+		let describe: Combinator<Alphabet> -> String = fixpoint("S") { recur, combinator in
+			switch combinator.language {
+			case .Empty:
+				return "∅"
+				
+			case let .Null(parses):
+				return "ε↓{\(parses.description)}"
+				
+				
+			case let .Literal(c):
+				return "'\(toString(c))'"
+				
+				
+			case let .Alternation(left, right):
+				return "(\(recur(left)) ∪ \(recur(right)))"
+				
+				
+			case let .Concatenation(first, second) where first == second:
+				return "\(recur(first))+"
+				
+			case let .Concatenation(first, second):
+				return "(\(recur(first)) ✕ \(recur(second)))"
+				
+				
+			case let .Repetition(language):
+				return "\(recur(language))*"
+				
+				
+			case let .Reduction(language, _):
+				return "\(recur(language)) → 𝑓"
+			}
+		}
 		return describe(self)
 	}
-}
-
-
-/// Pretty-prints the language represented by a combinator.
-func describe<Alphabet : Alphabet>(combinator: Combinator<Alphabet>) -> String {
-	let describe: Combinator<Alphabet> -> String = fixpoint("S") { recur, combinator in
-		switch combinator.language {
-		case .Empty:
-			return "∅"
-			
-		case let .Null(parses):
-			return "ε↓{\(parses.description)}"
-			
-			
-		case let .Literal(c):
-			return "'\(toString(c))'"
-			
-			
-		case let .Alternation(left, right):
-			return "(\(recur(left)) ∪ \(recur(right)))"
-			
-			
-		case let .Concatenation(first, second) where first == second:
-			return "\(recur(first))+"
-			
-		case let .Concatenation(first, second):
-			return "(\(recur(first)) ✕ \(recur(second)))"
-
-			
-		case let .Repetition(language):
-			return "\(recur(language))*"
-			
-			
-		case let .Reduction(language, _):
-			return "\(recur(language)) → 𝑓"
-		}
-	}
-	return describe(combinator)
 }
